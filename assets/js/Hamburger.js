e6c71e5720144b7e9a724c004dd5d4c191bcd253
daniel <danielcourot@hotmail.com>
2024-10-23 17:17:58 +0200

    change agenda

--- assets/js/Hamburger.js
+++ assets/js/Hamburger.js
@@ -2,6 +2,6 @@ document.getElementById('menu-toggle').addEventListener('click', function() {
     const navbar = document.getElementById('navbar-sticky');
     navbar.classList.toggle('hidden');
 
-    console.log("dddd");
+    // console.log("dddd");
     
 });
